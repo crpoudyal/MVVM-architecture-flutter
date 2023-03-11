8641ff706bb564b104ac8ce27dde07e6676d2c75
crpoudyal <crpoudyal@gmail.com>
2023-03-11 09:23:29 +0545

    feat: utils implemented in seperate file

---
+++ lib/utils/utils.dart
@@ -0,0 +1,30 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+  static toastMessage(String message) {
+    Fluttertoast.showToast(msg: message);
+  }
+
+  static flashBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        forwardAnimationCurve: Curves.decelerate,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.all(20),
+        message: message,
+        backgroundColor: Colors.red,
+        reverseAnimationCurve: Curves.easeOut,
+        positionOffset: 20,
+        icon: const Icon(
+          Icons.error,
+          size: 28,
+          color: Colors.white,
+        ),
+      )..show(context),
+    );
+  }
+}
