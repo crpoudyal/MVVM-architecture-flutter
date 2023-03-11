91ef69ac1cde93769575ed218d69c10c9c17102a
crpoudyal <crpoudyal@gmail.com>
2023-03-11 16:17:46 +0545

    feat: login ui updated

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,18 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_architecture_flutter/res/components/round_button.dart';
+import 'package:mvvm_architecture_flutter/utils/utils.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginViewState extends State<LoginView> {
+  final ValueNotifier<bool> _obscurePassword = ValueNotifier<bool>(true);
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _emailController.dispose();
+    _passwordController.dispose();
+
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                focusNode: _emailFocusNode,
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value) {
+                  Utils.fieldFocusChange(
+                      context, _emailFocusNode, _passwordFocusNode);
+                },
+              ),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              ValueListenableBuilder(
+                  valueListenable: _obscurePassword,
+                  builder: (context, value, child) {
+                    return TextFormField(
+                      controller: _passwordController,
+                      focusNode: _passwordFocusNode,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: const Icon(Icons.lock_outline),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            _obscurePassword.value = !_obscurePassword.value;
+                          },
+                          child: _obscurePassword.value
+                              ? const Icon(Icons.visibility_off_outlined)
+                              : const Icon(Icons.visibility_outlined),
+                        ),
+                      ),
+                      obscureText: _obscurePassword.value,
+                    );
+                  }),
+              SizedBox(
+                height: height * 0.1,
+              ),
+              RoundButton(
+                title: 'Login',
+                onPress: () {
+                  if (_emailController.text.isEmpty) {
+                    Utils.flashBarErrorMessage(
+                        "Please enter your email", context);
+                  } else if (_passwordController.text.isEmpty) {
+                    Utils.flashBarErrorMessage(
+                        "Please enter your password", context);
+                  } else if (_passwordController.text.length < 6) {
+                    Utils.flashBarErrorMessage(
+                        "Please enter 6 digit password", context);
+                  } else {}
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
