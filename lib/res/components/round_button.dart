66ee5eb87015ffa7829203a379af89bd67f8ce35
crpoudyal <crpoudyal@gmail.com>
2023-03-11 16:17:00 +0545

    feat: resources added and created components for app

---
+++ lib/res/components/round_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_architecture_flutter/res/app_colors.dart';
+
+class RoundButton extends StatelessWidget {
+  final String title;
+  final bool loading;
+  final VoidCallback onPress;
+  const RoundButton(
+      {Key? key,
+      required this.title,
+      this.loading = false,
+      required this.onPress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        height: 40,
+        width: 200,
+        decoration: BoxDecoration(
+          color: AppColors.buttonColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+            child: loading
+                ? const CircularProgressIndicator()
+                : Text(
+                    title,
+                    style: const TextStyle(
+                        color: AppColors.whiteColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18),
+                  )),
+      ),
+    );
+  }
+}
